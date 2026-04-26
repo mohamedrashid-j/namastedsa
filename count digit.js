83aafc456d00be579ba305eccd2a9adf69e31602
Mohamed Rashid <jmrmohamedrashid@gmail.com>
2026-04-26 12:24:38 +0000

    count digit modified

--- count digit.js
+++ count digit.js
@@ -1,10 +1,14 @@
-var a = 0;
-
-let count = 0;
-let n = 129293939311;
-while (n > 0) {
+function countDigits(n) {
+  if (n === 0) return 1;
+  let count = 0;
+  n = Math.abs(n);
+  while (n > 0) {
     n = Math.floor(n / 10);
     count++;
+  }
+  return count;
 }
 
-console.log(count)
+let n = 1992929;
+let result = countDigits(n);
+console.log(result);
